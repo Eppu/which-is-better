9b33f98aa57815715e8168cd32779d8e26b555ee
Eetu Eskelinen <edwardtehgreat@gmail.com>
2021-01-12 23:12:09 +0200

    Clean up function

--- functions/updateVotes/updateVotes.js
+++ functions/updateVotes/updateVotes.js
@@ -44,7 +44,6 @@ const handler = async (event) => {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
       },
       body: JSON.stringify({ message: `Updating votecount for ${subject}` }),
     }
