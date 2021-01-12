92838931f7bffe0c9effa3bf831392cbb31860e2
Eetu Eskelinen <edwardtehgreat@gmail.com>
2021-01-12 16:14:16 +0200

    Modify CORS again...

--- functions/updateVotes/updateVotes.js
+++ functions/updateVotes/updateVotes.js
@@ -42,7 +42,9 @@ const handler = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
       },
       body: JSON.stringify({ message: `Updating votecount for ${subject}` }),
     }
--- functions/words/words.js
+++ functions/words/words.js
@@ -16,7 +16,9 @@ exports.handler = function (event, context, callback) {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
       },
       body: JSON.stringify(body)
     });
